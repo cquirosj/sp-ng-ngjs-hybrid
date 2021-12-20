cb910dc83e8d5019125b136ec09883efc570b563
Christian <christian.quiros@particular.net>
2021-12-19 18:40:42 -0600

    just adding servicecontrol original source

---
+++ apps/pulse/src/app/js/app.constants.js
@@ -0,0 +1,7 @@
+window.defaultConfig = {
+    default_route: '/dashboard',
+    version: '1.2.0',
+    service_control_url: 'http://christianqufe6a:49201/api/',
+    monitoring_urls: ['http://christianqufe6a:49205/'],
+    showPendingRetry: false
+};
