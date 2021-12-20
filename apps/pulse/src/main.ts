5bb6ac3356704694e3fb81882a0140aeeed25b03
Christian <christian.quiros@particular.net>
2021-12-19 18:31:20 -0600

    angular-route loading and working with types

--- apps/pulse/src/main.ts
+++ apps/pulse/src/main.ts
@@ -1,15 +1,22 @@
-import { enableProdMode, ViewEncapsulation } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+//import * as angular from 'angular';
+import {module} from 'angular';
+import 'angular-route';
+//import { IRouteProvider } from 'angular-route';
 
-if (environment.production) {
-  enableProdMode();
-}
+//import { IRouteService }  from 'angular.route';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule, {
-    defaultEncapsulation: ViewEncapsulation.None,
-  })
-  .catch((err) => console.error(err));
+/// <reference path="../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../typings/angularjs/angular-route.d.ts" />
+
+export default module('testApp', ['ngRoute'])
+.config(['$routeProvider', function ($routeProvider:  angular.route.IRouteProvider) {
+  $routeProvider.when('/Cart', { templateUrl: '/partials/Cart.html', controller: 'checkoutSimulatorCtrl' });
+  $routeProvider.otherwise({ redirectTo: '/Cart' });
+}])
+.controller('checkoutSimulatorCtrl', [function () {
+  console.log('Controller initialized')
+}])
+.run(['$rootScope', function () {
+  console.log('angularjs bootstraped'); 
+}]);
