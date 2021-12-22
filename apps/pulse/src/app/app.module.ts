43d4e09cdce19d401cd0836bccae8f539f6bb1e4
Christian <christian.quiros@particular.net>
2021-12-22 15:27:01 -0600

    Bootstraping AngularJs from NG

---
+++ apps/pulse/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { UpgradeModule } from '@angular/upgrade/static';
+import * as angular from 'angular';
+import { ConnectionsManager } from './modules/configuration/connectionsManager';
+//import { AppComponent } from './app.component';
+//import { NxWelcomeComponent } from './nx-welcome.component';
+
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule,UpgradeModule],
+  providers: [],
+  bootstrap: [],
+})
+export class AppModule {
+  constructor(private upgrade: UpgradeModule) {}
+  ngDoBootstrap() {
+    var serviceControlApp = angular.module('sc');
+
+    var injector = angular.injector(['ng']);
+    var $http = injector.get('$http');
+    var connectionManager: ConnectionsManager = (<any>window)
+      .connectionsManager;
+    var scUrl = connectionManager.getServiceControlUrl();
+    console.debug('Retrieving license from ServiceControl at: ', scUrl);
+    var _self = this;
+    $http
+      .get(scUrl + '/license')
+      .then(
+        function (response) {
+          serviceControlApp.constant('license', response.data);
+        },
+        function () {
+          serviceControlApp.constant('license', {
+            license_status: 'Unavailable',
+          });
+        }
+      )
+      .then(function () {
+        angular.element(document).ready(function () {
+          //angular.bootstrap(document, ['sc']);
+          _self.upgrade.bootstrap(document.documentElement, ['sc']);
+        });
+      });
+  }
+}
---
+++ apps/pulse/src/main-ngjs.ts
@@ -0,0 +1,116 @@
+import './app/js/app.constants' // <script src="js/app.constants.js?v=1621602441174"></script>
+import './app/modules/shell/shell' //     <script src="modules/dist/shell.dist.js?v=1621602441174"></script>
+
+import './app/modules/monitoring/monitoring' //     <script src="modules/dist/monitoring.dist.js?v=1621602441174"></script>
+import './app/modules/configuration/configuration' //     <script src="modules/dist/configuration.dist.js?v=1621602441174"></script>
+
+import './app/lib/page-width-functions' //     <script src="lib/page-width-functions.js?v=1621602441174"></script>
+
+//     <!-- App -->
+import './app/js/app' //     <script src="js/app.js?v=1621602441174"></script>
+import './app/js/app.route' //     <script src="js/app.route.js?v=1621602441174"></script>
+import './app/js/app.controller' //     <script src="js/app.controller.js?v=1621602441174"></script>
+import './app/js/app.logging' //     <script src="js/app.logging.js?v=1621602441174"></script>
+import './app/js/app.http' //     <script src="js/app.http.js?v=1621602441174"></script>
+//import './app/js/app.bootstrap' //     <script src="js/app.bootstrap.js?v=1621602441174"></script>
+import './app/js/components/sc_not_available.component'
+
+//     <!-- Services -->
+import './app/js/services/services.module' //     <script src="js/services/services.module.js?v=1588195944426"></script>
+import './app/js/services/services.exception-handler' //     <script src="js/services/services.exception-handler.js?v=1588195944426"></script>
+import './app/js/services/services.notifications'//     <script src="js/services/services.notifications.js?v=1588195944426"></script>
+import './app/js/services/services.platform-update'//     <script src="js/services/services.platform-update.js?v=1588195944426"></script>
+import './app/js/services/services.semver' //     <script src="js/services/services.semver.js?v=1588195944426"></script>
+import './app/js/services/services.service-control' //     <script src="js/services/services.service-control.js?v=1588195944426"></script>
+import './app/js/services/services.stream' //     <script src="js/services/services.stream.js?v=1588195944426"></script>
+import './app/js/services/services.uri' //     <script src="js/services/services.uri.js?v=1588195944426"></script>
+import './app/js/services/services.export' //     <script src="js/services/services.export.js?v=1588195944426"></script>
+import './app/js/services/services.historyperiods' //     <script src="js/services/services.historyperiods.js?v=1588195944426"></script>
+import './app/js/services/factory.listener' //     <script src="js/services/factory.listener.js?v=1588195944426"></script>
+import './app/js/services/factory.notifier' //     <script src="js/services/factory.notifier.js?v=1588195944426"></script>
+import './app/js/services/factory.shareddata'//     <script src="js/services/factory.shareddata.js?v=1588195944426"></script>
+import './app/js/services/services.endpoints' //     <script src="js/services/services.endpoints.js?v=1588195944426"></script>
+import './app/js/services/constant.rxjs' //     <script src="js/services/constant.rxjs.js?v=1588195944426"></script>
+import './app/js/services/service.formatter' //     <script src="js/services/service.formatter.js?v=1588195944426"></script>
+
+//     <!-- Directives -->
+import './app/js/directives/ngClip' //     <script src="js/directives/ngClip.js?v=1621602441174"></script>
+import './app/js/directives/ngHighlight' //     <script src="js/directives/ngHighlight.js?v=1621602441174"></script>
+import './app/js/directives/moment'//     <script src="js/directives/moment.js?v=1621602441174"></script>
+import './app/js/directives/ui.particular.hud'//     <script src="js/directives/ui.particular.hud.js?v=1621602441174"></script>
+import './app/js/directives/ui.particular.busy'//     <script src="js/directives/ui.particular.busy.js?v=1621602441174"></script>
+import './app/js/directives/ui.particular.nodata'//     <script src="js/directives/ui.particular.nodata.js?v=1621602441174"></script>
+import './app/js/directives/ui.particular.exclamation'//     <script src="js/directives/ui.particular.exclamation.js?v=1621602441174"></script>
+import './app/js/directives/ui.particular.confirmclick' //     <script src="js/directives/ui.particular.confirmclick.js?v=1621602441174"></script>
+import './app/js/directives/ui.particular.tabset'//     <script src="js/directives/ui.particular.tabset.js?v=1621602441174"></script>
+import './app/js/directives/ui.particular.tab'//     <script src="js/directives/ui.particular.tab.js?v=1621602441174"></script>
+import './app/js/directives/ui.particular.eatclick'//     <script src="js/directives/ui.particular.eatclick.js?v=1621602441174"></script>
+import './app/js/directives/ui.particular.failedMessageTabs'//     <script src="js/directives/ui.particular.failedMessageTabs.js?v=1621602441174"></script>
+import './app/js/directives/ui.particular.largenumber'//     <script src="js/directives/ui.particular.largenumber.js?v=1621602441174"></script>
+import './app/js/directives/ui.particular.multi-checkboxlist' //     <script src="js/directives/ui.particular.multi-checkboxlist.js?v=1621602441174"></script>
+import './app/js/directives/ui.particular.pageTitleFromRoute' //     <script src="js/directives/ui.particular.pageTitleFromRoute.js?v=1621602441174"></script>
+import './app/js/directives/ui.particular'//     <script src="js/directives/ui.particular.js?v=1621602441174"></script>
+
+//     <!-- Mystery -->
+import './app/js/views/event_log_items/eventLogItems' //     <script src="js/views/event_log_items/eventLogItems.js?v=1621602441174"></script>
+
+//     <!-- Archive -->
+import './app/js/views/about/route' //     <script src="js/views/about/route.js?v=1621602441174"></script>
+import './app/js/views/about/controller' //     <script src="js/views/about/controller.js?v=1621602441174"></script>
+
+
+//     <!-- Dashboard -->
+import './app/js/views/dashboard/dashboard.module' //     <script src="js/views/dashboard/dashboard.module.js?v=1621602441174"></script>
+import './app/js/views/dashboard/dashboard.route' //     <script src="js/views/dashboard/dashboard.route.js?v=1621602441174"></script>
+import './app/js/views/dashboard/dashboard.controller' //     <script src="js/views/dashboard/dashboard.controller.js?v=1621602441174"></script>
+
+//     <!-- Failed Groups -->
+import './app/js/views/failed_groups/route' //     <script src="js/views/failed_groups/route.js?v=1621602441174"></script>
+import './app/js/views/failed_groups/controller'//     <script src="js/views/failed_groups/controller.js?v=1621602441174"></script>
+import './app/js/views/failed_groups/service'//     <script src="js/views/failed_groups/service.js?v=1621602441174"></script>
+
+//     <!-- Failed Messages -->
+import './app/js/views/failed_messages/route'//     <script src="js/views/failed_messages/route.js?v=1621602441174"></script>
+import './app/js/views/failed_messages/controller'//     <script src="js/views/failed_messages/controller.js?v=1621602441174"></script>
+import './app/js/views/failed_messages/service'//     <script src="js/views/failed_messages/service.js?v=1621602441174"></script>
+
+//     <!-- Failed Message -->
+import './app/js/views/message/route'//     <script src="js/views/message/route.js?v=1621602441174"></script>
+import './app/js/views/message/controller'//     <script src="js/views/message/controller.js?v=1621602441174"></script>
+//     <!-- Failed Message Editor -->
+import './app/js/views/message/editor/messageEditorModal.service'//     <script src="js/views/message/editor/messageEditorModal.service.js?v=1621602441174"></script>
+import './app/js/views/message/editor/messageEditorModal.controller'//     <script src="js/views/message/editor/messageEditorModal.controller.js?v=1621602441174"></script>
+
+//     <!-- Archive -->
+import './app/js/views/archive/route'//     <script src="js/views/archive/route.js?v=1621602441174"></script>
+import './app/js/views/archive/controller'//     <script src="js/views/archive/controller.js?v=1621602441174"></script>
+import './app/js/views/archive/service'//     <script src="js/views/archive/service.js?v=1621602441174"></script>
+
+//     <!-- Archive Groups -->
+import './app/js/views/archivedgroups/archivedgroups.route'//     <script src="js/views/archivedgroups/archivedgroups.route.js?v=1621602441174"></script>
+import './app/js/views/archivedgroups/archivedgroups.controller'//     <script src="js/views/archivedgroups/archivedgroups.controller.js?v=1621602441174"></script>
+import './app/js/views/archivedgroups/archivedgroups.service'//     <script src="js/views/archivedgroups/archivedgroups.service.js?v=1621602441174"></script>
+
+//     <!-- Pending Retries -->
+import './app/js/views/pending_retries/route'//     <script src="js/views/pending_retries/route.js?v=1621602441174"></script>
+import './app/js/views/pending_retries/controller'//     <script src="js/views/pending_retries/controller.js?v=1621602441174"></script>
+import './app/js/views/pending_retries/service'//     <script src="js/views/pending_retries/service.js?v=1621602441174"></script>
+
+//     <!-- Custom Checks -->
+import './app/js/views/custom_checks/customChecks.module' //     <script src="js/views/custom_checks/customChecks.module.js?v=1621602441174"></script>
+import './app/js/views/custom_checks/customChecks.route' //     <script src="js/views/custom_checks/customChecks.route.js?v=1621602441174"></script>
+import './app/js/views/custom_checks/customChecks.controller' //     <script src="js/views/custom_checks/customChecks.controller.js?v=1621602441174"></script>
+
+//     <!-- Endpoints -->
+import './app/js/views/endpoints/endpoints.module' //     <script src="js/views/endpoints/endpoints.module.js?v=1621602441174"></script>
+import './app/js/views/endpoints/endpoints.route'//     <script src="js/views/endpoints/endpoints.route.js?v=1621602441174"></script>
+import './app/js/views/endpoints/endpoints.controller'//     <script src="js/views/endpoints/endpoints.controller.js?v=1621602441174"></script>
+
+//     <!-- Comments -->
+import './app/js/views/comments/commentmodal.service'//     <script src="js/views/comments/commentmodal.service.js?v=1621602441174"></script>
+import './app/js/views/comments/comment.controller'//     <script src="js/views/comments/comment.controller.js?v=1621602441174"></script>
+
+//     <!--Polyfills-->
+import './app/js/polyfill/array.prototype.find'//     <script src="js/polyfill/array.prototype.find.js?v=1621602441174"></script>
+import './app/js/polyfill/array.prototype.includes'//     <script src="js/polyfill/array.prototype.includes.js?v=1621602441174"></script>
+import './app/js/polyfill/string.prototype.endsWith'//     <script src="js/polyfill/string.prototype.endsWith.js?v=1621602441174"></script>
